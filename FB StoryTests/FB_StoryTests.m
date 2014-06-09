2c22ba67a6ece39b1e988032235d27c7bb6ffea3
mattstudio <matt@usertesting.com>
2014-06-09 09:20:01 -0700

    Initial Commit

---
+++ FB StoryTests/FB_StoryTests.m
@@ -0,0 +1,34 @@
+//
+//  FB_StoryTests.m
+//  FB StoryTests
+//
+//  Created by Matt Mitchell on 6/9/14.
+//  Copyright (c) 2014 Matt Mitchell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FB_StoryTests : XCTestCase
+
+@end
+
+@implementation FB_StoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
